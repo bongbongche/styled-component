b1d63a15719c6aa404536741d43d8a8a2ff8d385
bongbong <60419514+bongbongche@users.noreply.github.com>
2020-06-09 18:50:42 +0900

    styled-component practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding:0;
+    margin:0;
+  }
+`;
+
+// const awesomeCard = css`
+//   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+//   background-color: white;
+//   border-radius: 10px;
+//   padding: 20px;
+//   border: none;
+// `;
+
+const Card = styled.div`
+  background-color: white;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+  ${Card} {
+    background-color: aqua;
+  }
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Container>
+            <Form />
+          </Container>
+        </ThemeProvider>
+      </Fragment>
+    );
+  }
 }
 
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${(props) => props.theme.successColor};
+`;
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 export default App;
